6adceeb63c958a8719fe8ea9e3d3ad91bf8bd67c
Aristote-code <gahimaaristote1@gmail.com>
2024-09-09 21:12:10 +0200

    Updating multiple files

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -2,7 +2,6 @@
 """LFU cache"""
 
 from base_caching import BaseCaching
-from collections import defaultdict
 
 
 class LFUCache(BaseCaching):
@@ -11,43 +10,59 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """Initialize"""
         super().__init__()
-        self.frequency = defaultdict(int)
-        self.usage_order = []
+        self.frequency = {}
+        self.min_frequency = 0
+        self.frequency_list = {}
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            self._discard_least_frequent()
-
-        self.cache_data[key] = item
-        self.frequency[key] += 1
-        self._update_usage_order(key)
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self._update_frequency(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            self._evict()
+            self.cache_data[key] = item
+            self.frequency[key] = 1
+            self.min_frequency = 1
+            if 1 not in self.frequency_list:
+                self.frequency_list[1] = []
+            self.frequency_list[1].append(key)
+        else:
+            self.cache_data[key] = item
+            self.frequency[key] = 1
+            self.min_frequency = 1
+            if 1 not in self.frequency_list:
+                self.frequency_list[1] = []
+            self.frequency_list[1].append(key)
 
     def get(self, key):
         """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
-
-        self.frequency[key] += 1
-        self._update_usage_order(key)
+        self._update_frequency(key)
         return self.cache_data[key]
 
-    def _discard_least_frequent(self):
-        """Discard the least frequently used item"""
-        min_freq = min(self.frequency.values())
-        least_frequent = [k for k in self.usage_order if self.frequency[k] == min_freq]
+    def _update_frequency(self, key):
+        """Update frequency of an item"""
+        freq = self.frequency[key]
+        self.frequency[key] += 1
+        self.frequency_list[freq].remove(key)
+        if not self.frequency_list[freq]:
+            del self.frequency_list[freq]
+            if freq == self.min_frequency:
+                self.min_frequency += 1
+        if freq + 1 not in self.frequency_list:
+            self.frequency_list[freq + 1] = []
+        self.frequency_list[freq + 1].append(key)
 
-        discard_key = least_frequent[0]
-        del self.cache_data[discard_key]
-        del self.frequency[discard_key]
-        self.usage_order.remove(discard_key)
-        print("DISCARD:", discard_key)
-
-    def _update_usage_order(self, key):
-        """Update the usage order of items"""
-        if key in self.usage_order:
-            self.usage_order.remove(key)
-        self.usage_order.append(key)
+    def _evict(self):
+        """Evict the least frequently used item"""
+        lfu_key = self.frequency_list[self.min_frequency].pop(0)
+        if not self.frequency_list[self.min_frequency]:
+            del self.frequency_list[self.min_frequency]
+        del self.cache_data[lfu_key]
+        del self.frequency[lfu_key]
+        print(f"DISCARD: {lfu_key}")
