a7afd9d8a7af55c87566b8c6a856126984488e25
Aristote-code <gahimaaristote1@gmail.com>
2024-09-13 13:34:19 +0200

    Updating multiple files

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -2,7 +2,7 @@
 """ LFU Caching """
 
 from base_caching import BaseCaching
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
@@ -11,49 +11,51 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize the LFUCache """
         super().__init__()
-        self.frequency = defaultdict(int)  # Tracks frequency of each key
-        self.usage_order = OrderedDict()   # Tracks the order of usage (LRU)
-    
+        self.frequency = {}
+        self.usage_order = OrderedDict()
+
     def put(self, key, item):
         """ Add an item to the cache """
         if key is None or item is None:
             return
-        
+
         if key in self.cache_data:
-            # Update existing item, increment its frequency
+            # Update the item and increase frequency
             self.cache_data[key] = item
             self.frequency[key] += 1
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 self._evict()
-            # Add new item with initial frequency of 1
+            # Add the new item with initial frequency 1
             self.cache_data[key] = item
             self.frequency[key] = 1
-        
-        # Update usage order to reflect the recent usage of the key
-        if key in self.usage_order:
-            del self.usage_order[key]
-        self.usage_order[key] = None  # Insert or move key to the end
+        # Update usage order
+        self.usage_order[key] = None
+        self.usage_order.move_to_end(key)
 
     def get(self, key):
         """ Retrieve an item by key """
         if key is None or key not in self.cache_data:
             return None
-        
         # Increment frequency and update usage order
         self.frequency[key] += 1
         self.usage_order.move_to_end(key)
         return self.cache_data[key]
 
     def _evict(self):
-        """ Evict the least frequently used item. If tie, evict LRU """
-        # Find the minimum frequency among the items
+        """
+        Evict the least frequently used item.
+        If there's a tie, evict the least recently used item among them.
+        """
+        # Find the minimum frequency among all keys
         min_freq = min(self.frequency.values())
-        # Find all keys with this minimum frequency
-        min_freq_keys = [k for k in self.usage_order if self.frequency[k] == min_freq]
-
-        # Evict the least recently used among the keys with minimum frequency
+        # Identify keys with the minimum frequency
+        min_freq_keys = [
+            k for k in self.usage_order if self.frequency[k] == min_freq
+        ]
+        # The first key in usage_order among min_freq_keys is the LRU
         discard_key = min_freq_keys[0]
+        # Remove the item from all tracking structures
         del self.cache_data[discard_key]
         del self.frequency[discard_key]
         del self.usage_order[discard_key]
