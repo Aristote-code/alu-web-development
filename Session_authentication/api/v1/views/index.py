c45d16c3e1fd41bef99b0b7aa74b15debf89f7b4
Aristote-code <gahimaaristote1@gmail.com>
2024-09-15 21:52:14 +0200

    Updatimng multiple files

--- Session_authentication/api/v1/views/index.py
+++ Session_authentication/api/v1/views/index.py
@@ -11,35 +11,20 @@ def status() -> str:
     Return:
       - the status of the API
     """
-    print("Status endpoint called")  # Add this line for debugging
-    return jsonify({"status": "OK"})
+    return "OK"
 
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
-
-
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
-      - abort(401)
+      - Unauthorized error
     """
     abort(401)
 
-
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
-      - abort(403)
+      - Forbidden error
     """
     abort(403)
