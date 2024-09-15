b67b6b7dfaeed0951c4cdd4b7116426190065684
Aristote-code <gahimaaristote1@gmail.com>
2024-09-15 21:55:27 +0200

    Updated

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -3,16 +3,14 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify
-from api.v1.views import app_views
+from flask import Flask
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 
 @app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns API status """
-    return jsonify({"status": "OK"})
+    return "OK"
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
