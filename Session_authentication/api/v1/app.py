d2a6d145cffabf11e57c6e03a8f48d5dab90cee0
Aristote-code <gahimaaristote1@gmail.com>
2024-09-15 21:57:04 +0200

    Update

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -3,14 +3,16 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
 
 @app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns API status """
-    return "OK"
+    response = make_response("OK")
+    response.headers['Content-Type'] = 'text/plain'
+    return response
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
